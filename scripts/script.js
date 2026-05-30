d08ca6d88be089563da3ecdf6685cb0d0a022bc6
MichelleG045 <114456915+MichelleG045@users.noreply.github.com>
2026-05-29 21:57:56 -0700

    change to soundcloud

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,24 @@
-const guitarContainer1 = document.getElementById("guitar-container-1");
-const guitarContainer2 = document.getElementById("guitar-container-2");
-const musicPlayer = document.getElementById("music-player");
+document.addEventListener("DOMContentLoaded", function () {
+  const guitarContainer = document.getElementById("guitar-container-1");
+  const musicPlayer = document.getElementById("music-player");
 
-// get all skills
+  if (!guitarContainer || !musicPlayer || typeof SC === "undefined") {
+    return;
+  }
 
-// function generatePDF() {
+  const widget = SC.Widget(musicPlayer);
+  let widgetIsReady = false;
 
-//   console.log('export pdf format to be configured...')
+  widget.bind(SC.Widget.Events.READY, function () {
+    widgetIsReady = true;
+    widget.setVolume(40);
+  });
 
-//   return;
+  guitarContainer.addEventListener("click", function () {
+    if (!widgetIsReady) {
+      return;
+    }
 
-//   var element = document.getElementById('CV');
-
-//   // TODO: configure this options object to export correct pdf
-//   var options = {
-//     margin: 1, 
-//     filename: 'maksym-polinka.pdf',
-//     image: { type: 'jpeg', quality: 1 },
-//     html2canvas: { scale: 2 },
-//     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait', precision: '12' }
-//   }
-
-//   html2pdf().set(options).from(element).save();
-// }
-
-$(document).ready(
-  (function () {
-    var iframeElement = document.querySelector("iframe");
-    var widget = SC.Widget(iframeElement);
-    // api methods -- https://developers.soundcloud.com/docs/api/html5-widget#methods
-    
-    guitarContainer1.addEventListener("click", () => {
-      widget.setVolume(40);
-      widget.toggle();
-    });
-
-    guitarContainer2.addEventListener("click", () => {
-      widget.setVolume(40);
-      widget.toggle();
-    });
-
-
-  })()
-);
+    widget.toggle();
+  });
+});
